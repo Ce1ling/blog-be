ec52481ccde31a779514f69ce670ae1c559d6497
Ce1ling <2134045201@qq.com>
2023-05-14 14:39:52 +0800

    fix: fix CORS position

--- src/app.js
+++ src/app.js
@@ -5,19 +5,19 @@ const app = express()
 
 const PORT = 3211
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-routes.forEach(route => app.use(route))
-app.use((_, response, next) => {
-  response.send({ code: -1, msg: '404 Not Found' })
-  next()
-})
-
 app.all('*', (request, response, next) => {
   response.header('Access-Control-Allow-Origin', request.headers.origin)
   response.header('Control-Access-Allowe-Methods', request.method)
   response.header('Control-Access-Allowe-Headers', 'Content-Type')
   response.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+routes.forEach(route => app.use(route))
+app.use((_, response, next) => {
+  response.send({ code: -1, msg: '404 Not Found' })
   next()
 })
 
