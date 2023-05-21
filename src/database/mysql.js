d6af29fd8dc24bccae542f895e143970c866d8c8
Ce1ling <2134045201@qq.com>
2023-05-21 19:49:09 +0800

    refactor: refactor connect database file

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -5,20 +5,27 @@ const connection = mysql.createConnection(MYSQL_CONFIG)
 
 connection.connect()
 
-const runSQL = (sql) => {
-  return new Promise((resolve, reject) => {
-    connection.query(sql, (error, result) => {
-      if (error) reject(error)
-      else resolve(result)
+const db = {
+  /**
+   * 运行 `SQL` 语句, 若您没有处理用户输入, 则可能会受到 `XSS` 攻击.
+   * @param {string} sql `SQL` 语句
+   */
+  run(sql) {
+    return new Promise((resolve, reject) => {
+      connection.query(sql, (error, result) => {
+        if (error) reject(error)
+        else resolve(result)
+      })
     })
-  })
-}
-
-const dbEnd = () => {
-  connection.end()
+  },
+  /**
+   * 关闭数据库连接
+   */
+  close() {
+    connection.end()
+  },
 }
 
 module.exports = {
-  runSQL,
-  dbEnd
+  db
 }
